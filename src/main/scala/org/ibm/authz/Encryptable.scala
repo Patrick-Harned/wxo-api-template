ed108c1ed5bd8229d9bef98dd641a05a9ea7f57c
Patrick Harned <pat@li-819025cc-34a5-11b2-a85c-ba699012569d.ibm.com>
2026-06-03 09:23:57 -0500

    using chunking to handle large strings

--- src/main/scala/org/ibm/authz/Encryptable.scala
+++ src/main/scala/org/ibm/authz/Encryptable.scala
@@ -1,58 +1,71 @@
 package org.ibm.authz
-
 import org.bouncycastle.jce.provider.BouncyCastleProvider
 import java.security.{KeyFactory, PrivateKey, PublicKey, Security}
-import java.security.spec.{PKCS8EncodedKeySpec, X509EncodedKeySpec}
+import java.security.interfaces.RSAPublicKey
 import javax.crypto.Cipher
 import javax.crypto.spec.OAEPParameterSpec
 import java.security.spec.MGF1ParameterSpec
 import java.util.Base64
-
+// Register provider once at object initialization
+private object CryptoSetup:
+  lazy val init: Unit =
+    if Security.getProvider("BC") == null then Security.addProvider(new BouncyCastleProvider())
 sealed trait EncryptionStrategy
-trait SHA1   extends EncryptionStrategy
-trait SHA256 extends EncryptionStrategy
+sealed abstract class SHA1 private[authz] ()   extends EncryptionStrategy
+sealed abstract class SHA256 private[authz] () extends EncryptionStrategy
 trait Encryptable[A <: EncryptionStrategy]:
   def encrypt(input: String, publicKey: PublicKey): String
-
 object Encryptable:
+  private def chunkSize(keyBitLength: Int, hashBytes: Int): Int =
+    (keyBitLength / 8) - 2 - (2 * hashBytes)
+  private def encryptChunked(
+      dataBytes: Array[Byte],
+      publicKey: PublicKey,
+      cipher: Cipher,
+      oaepParams: OAEPParameterSpec,
+      maxChunk: Int
+  ): Either[Throwable, String] =
+    scala.util.Try {
+      val encrypted = dataBytes
+        .grouped(maxChunk)
+        .map { chunk =>
+          cipher.init(Cipher.ENCRYPT_MODE, publicKey, oaepParams)
+          cipher.doFinal(chunk)
+        }
+        .foldLeft(Array.emptyByteArray)(_ ++ _)
+      Base64.getEncoder.encodeToString(encrypted)
+    }.toEither
   given sha1Encryptable: Encryptable[SHA1] with
     def encrypt(input: String, publicKey: PublicKey): String =
-      val dataBytes = input.getBytes("UTF-8")
-
-      val cipher     = Cipher.getInstance("RSA/ECB/OAEPWithSHA-1AndMGF1Padding")
+      CryptoSetup.init
       val oaepParams = new OAEPParameterSpec(
         "SHA-1",
         "MGF1",
         MGF1ParameterSpec.SHA1,
         OAEPParameterSpec.DEFAULT.getPSource
       )
-      cipher.init(Cipher.ENCRYPT_MODE, publicKey, oaepParams)
-
-      val encrypted = cipher.doFinal(dataBytes)
-      Base64.getEncoder.encodeToString(encrypted)
+      val cipher       = Cipher.getInstance("RSA/ECB/OAEPWithSHA-1AndMGF1Padding", "BC")
+      val keyBitLength = publicKey.asInstanceOf[RSAPublicKey].getModulus.bitLength()
+      val maxChunk     = chunkSize(keyBitLength, hashBytes = 20)
+      encryptChunked(input.getBytes("UTF-8"), publicKey, cipher, oaepParams, maxChunk)
+        .fold(e => throw e, identity)
   given sha256Encryptable: Encryptable[SHA256] with
     def encrypt(input: String, publicKey: PublicKey): String =
-      val dataBytes = input.getBytes("UTF-8")
-
-      val cipher = Cipher.getInstance("RSA/ECB/OAEPWithSHA-256AndMGF1Padding")
+      CryptoSetup.init
       val oaepParams = new OAEPParameterSpec(
         "SHA-256",
         "MGF1",
         MGF1ParameterSpec.SHA256,
         OAEPParameterSpec.DEFAULT.getPSource
       )
-      cipher.init(Cipher.ENCRYPT_MODE, publicKey, oaepParams)
-
-      val encrypted = cipher.doFinal(dataBytes)
-      Base64.getEncoder.encodeToString(encrypted)
-
-object EncryptionStrategy {
-
-  def fromEnv =
+      val cipher       = Cipher.getInstance("RSA/ECB/OAEPWithSHA-256AndMGF1Padding", "BC")
+      val keyBitLength = publicKey.asInstanceOf[RSAPublicKey].getModulus.bitLength()
+      val maxChunk     = chunkSize(keyBitLength, hashBytes = 32)
+      encryptChunked(input.getBytes("UTF-8"), publicKey, cipher, oaepParams, maxChunk)
+        .fold(e => throw e, identity)
+object EncryptionStrategy:
+  def fromEnv: Encryptable[? <: EncryptionStrategy] =
     sys.env.get("ENCRYPTION_STRATEGY") match
-      case Some("SHA1") =>
-        summon[Encryptable[SHA1]]
+      case Some("SHA1")   => summon[Encryptable[SHA1]]
       case Some("SHA256") => summon[Encryptable[SHA256]]
-      case _              =>
-        summon[Encryptable[SHA256]]
-}
+      case _              => summon[Encryptable[SHA256]]
